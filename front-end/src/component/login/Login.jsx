39ad72cdc2062618c88c97d344ca2d1a2483c14e
jakeshin89 <50133816+jakeshin89@users.noreply.github.com>
2019-12-22 02:46:56 +0900

    Update Login.jsx

--- front-end/src/component/login/Login.jsx
+++ front-end/src/component/login/Login.jsx
@@ -92,14 +92,14 @@ class Login extends Component{
     return(
       <div>
         <GoogleLogin 
-          clientId = "1006946931495-l762hspgg67tp5vrtq2q87cev7628rsk.apps.googleusercontent.com"
+          clientId = " 구글 key값 "
           buttonText = "Google 계정으로 로그인하기"
           onSuccess = {this.responseGoogle}
           onFailure = {this.responseFail}
         />
         <br/><br/>
         <KakaoButton
-          jsKey = "0c66144cffc02f5a068f08dd5ec47ffe"
+          jsKey = " 카카오 key값 "
           buttonText = "Kakao 계정으로 로그인하기"
           onSuccess = {this.responseKakao}
           onFailure = {this.responseFail}
